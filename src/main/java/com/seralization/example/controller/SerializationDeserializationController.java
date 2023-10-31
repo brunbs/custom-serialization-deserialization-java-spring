c137cff1637c3061def3827e5a3df05e06cd673b
Bruno Barbosa <babosa227@gmail.com>
2023-10-31 12:21:35 -0300

    add upper case serializer

--- src/main/java/com/seralization/example/controller/SerializationDeserializationController.java
+++ src/main/java/com/seralization/example/controller/SerializationDeserializationController.java
@@ -1,8 +1,12 @@
 package com.seralization.example.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.seralization.example.request.StudentRequest;
 import com.seralization.example.response.StudentResponse;
 import com.seralization.example.service.StudentService;
+import com.seralization.example.util.UpperCaseSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,8 +20,15 @@ public class SerializationDeserializationController {
     private StudentService studentService;
 
     @PostMapping("/students")
-    ResponseEntity<StudentResponse> createStudent(@RequestBody StudentRequest studentRequest) {
-        return ResponseEntity.ok().body(studentService.createStudent(studentRequest));
+    ResponseEntity<String> createStudent(@RequestBody StudentRequest studentRequest) throws JsonProcessingException {
+
+        StudentResponse student = studentService.createStudent(studentRequest);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new SimpleModule().addSerializer(new UpperCaseSerializer()));
+        String json = mapper.writeValueAsString(student);
+
+        return ResponseEntity.ok().body(json);
     }
 
 }
---
+++ src/main/java/com/seralization/example/util/UpperCaseSerializer.java
@@ -0,0 +1,25 @@
+package com.seralization.example.util;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+public class UpperCaseSerializer extends JsonSerializer<String> {
+
+
+    @Override
+    public void serialize(String s, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        if(s != null && !s.isEmpty() && !s.equals("null")) {
+            jsonGenerator.writeString(s.toUpperCase());
+        } else {
+            jsonGenerator.writeNull();
+        }
+    }
+
+    @Override
+    public Class<String> handledType() {
+        return String.class;
+    }
+}
